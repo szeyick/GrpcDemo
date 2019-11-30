a4262cf42008b3d64bc4d2e9ec85fec7693da7ea
Sze Yick <szeyick@gmail.com>
2019-11-29 22:04:18 -0500

    Test

--- src/main_test.go
+++ src/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -8,5 +9,24 @@ func TestSum(t *testing.T) {
 	total := Sum(5, 5)
 	if total != 10 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+	main()
+}
+
+func TestSomething(t *testing.T) {
+	tests := []struct {
+		desc string
+	}{
+		{
+			desc: "this is a test",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			t.Parallel()
+			total := Sum(5, 5)
+			assert.Equal(t, 10, total)
+		})
 	}
 }
